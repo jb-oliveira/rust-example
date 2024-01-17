5fd386feda358152d02de23ed6f160119c5ebbff
jb-oliveira <jbacos77@gmail.com>
2024-01-17 16:13:32 -0300

    connect to database

--- rust-crud-api/src/main.rs
+++ rust-crud-api/src/main.rs
@@ -5,13 +5,13 @@ use std::io::{Read, Write};
 use std::env;
 
 #[macro_use]
-extern crate  serde_derive;
+extern crate serde_derive;
 
-#[derive(Serialize,Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct User {
     id: Option<i32>,
     name: String,
-    email: String
+    email: String,
 }
 
 const DB_URL: &str = !env("DB_URL");
@@ -20,5 +20,20 @@ const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
 
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = set_database() {
+        println!("Error {}", e);
+        return;
+    }
+    
 }
+
+fn set_database() -> Result<(), PostgresError> {
+    let mut client = Client::connect(DB_URL, NoTls);
+    client.execute("CREATE TABLE IF NOT EXISTS users (\
+        id SERIAL PRIMARY KEY,\
+        name VARCHAR NOT NULL\
+        email VARCHAR NOT NULL\
+    ", &[])?
+}
+
+
