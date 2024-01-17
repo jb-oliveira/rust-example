a09d453932c05f0e1e43e86a60e6aaf399dc82dc
jb-oliveira <jbacos77@gmail.com>
2024-01-17 16:34:05 -0300

    added routes

--- rust-crud-api/src/main.rs
+++ rust-crud-api/src/main.rs
@@ -4,6 +4,7 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::env;
 use std::fmt::format;
+use serde_json::Value::String;
 
 #[macro_use]
 extern crate serde_derive;
@@ -25,8 +26,42 @@ fn main() {
         println!("Error {}", e);
         return;
     }
-    let listener =TcpListener::bind(format!(0.0.0.0:8080)).unwrap();
+    let listener = TcpListener::bind(format!(0.0.0.0:8080)).unwrap();
     println!("Server started!");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut request = String::new();
+
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
+            let (status_line, content) = match &request {
+                r if request_with("POST /users") => handle_post_request(r),
+                r if request_with("GET /users/") => handle_get_request(r),
+                r if request_with("GET /users") => handle_get_all_request(r),
+                r if request_with("PUT /users/") => handle_put_request(r),
+                r if request_with("DELETE /users/") => handle_delete_request(r),
+                _ => (NOT_FOUND, "Not found".to_string())
+            };
+            stream.write_all(format!("{}{}", status_line, content).as_bytes()).unwrap();
+        }
+        Err(e) => {
+            println!("Error {]}", e);
+        }
+    }
 }
 
 fn set_database() -> Result<(), PostgresError> {
@@ -38,4 +73,10 @@ fn set_database() -> Result<(), PostgresError> {
     ", &[])?
 }
 
+fn get_id(request: &str) -> &str {
+    request.split("/").nth(2).unwrap_or_default().split_ascii_whitespace().next().unwrap_or_default()
+}
 
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
+}
