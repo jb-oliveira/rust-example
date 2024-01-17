032b132cb9b0d967ab4d104138a3965359dfb1d4
jb-oliveira <jbacos77@gmail.com>
2024-01-17 15:29:36 -0300

    added constants

--- rust-crud-api/src/main.rs
+++ rust-crud-api/src/main.rs
@@ -14,6 +14,11 @@ struct User {
     email: String
 }
 
+const DB_URL: &str = !env("DB_URL");
+const OK_REPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+
 fn main() {
     println!("Hello, world!");
 }
