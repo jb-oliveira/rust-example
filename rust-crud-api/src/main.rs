85b397c6d0889ea3ae77897395fd88342efc143b
jb-oliveira <jbacos77@gmail.com>
2024-01-18 16:59:20 -0300

    handle delete request

--- rust-crud-api/src/main.rs
+++ rust-crud-api/src/main.rs
@@ -66,6 +66,21 @@ fn handle_client(mut stream: TcpStream) {
     }
 }
 
+fn handle_delete_request(request: &str) -> (String, String) {
+    match (get_id(&request).parse::<i32>(), Client::connect(DB_URL, NoTls)) {
+        (Ok(id), Ok(mut client)) => {
+            let rows_affected = client.execute("DELETE FROM users WHERE id = $1", &[&id]).unwrap();
+
+            if rows_affected == 0 {
+                return (NOT_FOUND.to_string(), "User not found".to_string());
+            }
+
+            (OK_REPONSE.to_string(), "User deleted".to_string())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    }
+}
+
 fn handle_put_request(request: &str) -> (String, String) {
     match
     (
